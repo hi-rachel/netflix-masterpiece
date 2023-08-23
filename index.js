0d66247dc00ec16559f5efe035fa718cdfc8b646
goorm <goorm@Goorm-MacBook-Pro.local>
2023-08-23 18:40:50 +0900

    Make slide move and Fix btn, link, background

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ window.addEventListener('scroll', () => {
 });
 
 const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 2,
+  slidesPerView: 1,
   slidesPerGroup: 1,
   centeredSlides: true,
   loop: true,
@@ -19,29 +19,29 @@ const swiper = new Swiper('.swiper-container', {
   },
   breakpoints: {
     // when window width is >= 600px
+    // 600: {
+    //   slidesPerView: 2,
+    //   slidesPerGroup: 2,
+    //   spaceBetween: 5,
+    //   centeredSlides: true,
+    // },
+    // // when window width is >= 900px
     600: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 5,
-      centeredSlides: true,
-    },
-    // when window width is >= 900px
-    900: {
       slidesPerView: 3,
       slidesPerGroup: 3,
-      spaceBetween: 5,
+      // spaceBetween: 5,
       centeredSlides: false,
     },
     // when window width is >= 1200px
     1200: {
       slidesPerView: 4,
       slidesPerGroup: 4,
-      spaceBetween: 5,
+      // spaceBetween: 5,
       centeredSlides: false,
     },
 
     // when window width is >= 1500px
-    1500: {
+    1480: {
       slidesPerView: 5,
       slidesPerGroup: 5,
       spaceBetween: 5,
@@ -52,7 +52,7 @@ const swiper = new Swiper('.swiper-container', {
     1800: {
       slidesPerView: 6,
       slidesPerGroup: 6,
-      spaceBetween: 5,
+      // spaceBetween: 5,
       centeredSlides: false,
     },
   },
